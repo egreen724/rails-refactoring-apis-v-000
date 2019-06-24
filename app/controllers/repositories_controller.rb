ba930c15f89c8e67d39b3bfc3585f3e768d423fe
Erin Green <erin.y.green@gmail.com>
2019-06-24 20:04:07 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,13 +1,15 @@
 class RepositoriesController < ApplicationController
   def index
     github = GithubService.new
-
+    github.access_token = session[:token]
     @repos_array = github.get_repos
 
   end
 
   def create
     github = GithubService.new
+    github.access_token = session[:token]
+    
     github.create_repo(params[:name])
     redirect_to '/'
   end
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,13 +1,13 @@
 class GithubService
 
-  attr_reader :access_token
+  attr_accessor :access_token
 
   def initialize(access_hash = nil)
     @access_token = access_hash["access_token"] if access_hash
   end
 
   def authenticate!(client_id, client_secret, code)
-    response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: client_id, client_secret: client_secret,code: code}, {'Accept' => 'application/json'}
+    response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: client_id, client_secret: client_secret, code: code}, {'Accept' => 'application/json'}
     access_hash = JSON.parse(response.body)
     @access_token = access_hash["access_token"]
 
@@ -23,6 +23,7 @@ class GithubService
     response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
     array = JSON.parse(response.body)
     array.map{ |repo| GithubRepo.new(repo)  }
+
   end
 
   def create_repo(name)
