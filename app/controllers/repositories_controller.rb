072159218f43596a87436f9c09db4ccbce166947
Erin Green <erin.y.green@gmail.com>
2019-06-24 19:56:14 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,7 +2,7 @@ class RepositoriesController < ApplicationController
   def index
     github = GithubService.new
     binding.pry
-    github.access_token = session[:token])
+    github.access_token = session[:token]
     @repos_array = github.get_repos
 
   end
