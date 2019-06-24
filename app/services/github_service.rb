c60b3b34ab92f0e517db6ed207f613d0cada5420
Erin Green <erin.y.green@gmail.com>
2019-06-24 19:28:57 +0000

    Automatically backed up by Learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -2,8 +2,9 @@ class GithubService
 
   attr_reader :access_token
 
-  def initialize(access_hash = nil)
-    @access_token = access_hash["access_token"] if access_hash
+  def initialize
+    # (access_hash = nil)
+    # @access_token = access_hash["access_token"] if access_hash
   end
 
   def authenticate!(client_id, client_secret, code)
